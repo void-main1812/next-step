2b0407dbf83ca1e1bcdb7b453fb39b256aa9bff4
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-07-17 20:40:35 +0530

    Update: Redirection URL after social Auth

--- App.tsx
+++ App.tsx
@@ -4,47 +4,21 @@ import 'react-native-gesture-handler';
 
 import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
 import { useFonts } from 'expo-font';
-import * as SecureStore from 'expo-secure-store';
+import { ActivityIndicator } from 'react-native';
+import { useStyles } from 'react-native-unistyles';
+import { tokenCache } from 'utils/TokenCache';
 import RootStack from './src/navigation';
-
-const tokenCache = {
-  async getToken(key: string) {
-    try {
-      const item = await SecureStore.getItemAsync(key);
-      if (item) {
-        console.log(`${key} was used 🔐 \n`);
-      } else {
-        console.log('No values stored under key: ' + key);
-      }
-      return item;
-    } catch (error) {
-      console.error('SecureStore get item error: ', error);
-      await SecureStore.deleteItemAsync(key);
-      return null;
-    }
-  },
-  async saveToken(key: string, value: string) {
-    try {
-      return SecureStore.setItemAsync(key, value);
-    } catch (err) {
-      console.error('SecureStore set item error: ', err);
-      return;
-    }
-  },
-};
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export default function App({navigation}: any) {
+export default function App() {
+  const { theme } = useStyles();
+  
   const [fontsLoaded] = useFonts({
     'ClashDisplay-Semibold': require('./assets/fonts/ClashDisplay-Semibold.otf'),
     'ClashDisplay-Medium': require('./assets/fonts/ClashDisplay-Medium.otf'),
     'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.otf'),
   });
-
-  if (!fontsLoaded) {
-    return null;
-  }
 
   if (!publishableKey) {
     throw new Error(
@@ -52,6 +26,10 @@ export default function App({navigation}: any) {
     );
   }
 
+  if (!fontsLoaded) {
+    return <ActivityIndicator size={'large'} color={theme.components.Text.title} />;
+  }
+  
   return (
     <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
       <ClerkLoaded>
--- components/SocialAuthButton.tsx
+++ components/SocialAuthButton.tsx
@@ -1,14 +1,14 @@
 import { useOAuth } from '@clerk/clerk-expo';
 import * as WebBrowser from 'expo-web-browser';
-import React, { useEffect } from 'react';
-import { Image, Pressable } from 'react-native';
+import * as Linking from 'expo-linking';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Image, Pressable } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 import { height } from 'utils/Size';
 
+// creating a browser warm-up function to avoid delay
 export const useWarmUpBrowser = () => {
   useEffect(() => {
-    // Warm up the android browser to improve UX
-    // https://docs.expo.dev/guides/authentication/#improving-user-experience
     void WebBrowser.warmUpAsync();
     return () => {
       void WebBrowser.coolDownAsync();
@@ -21,19 +21,30 @@ WebBrowser.maybeCompleteAuthSession();
 type SocialAuthButtonProps = {
   icon: any;
   provider: 'oauth_google' | 'oauth_facebook' | 'oauth_github';
-}
+};
 
 const SocialAuthButton = ({ icon, provider }: SocialAuthButtonProps) => {
+  // handling loading state
+  const [loading, setLoading] = useState(false);
 
+  // warming up the browser to avoid delay
   useWarmUpBrowser();
 
+  // importing the startOAuthFlow function from the useOAuth hook and passing the provider
   const { startOAuthFlow } = useOAuth({ strategy: provider });
+  // getting the styles and theme from the useStyles hook
+  const { styles, theme } = useStyles(styleSheet);
 
+  // handling the onPress event
   const onPress = React.useCallback(async () => {
+    setLoading(true);
     try {
-      const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow();
+      const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow({
+        redirectUrl: Linking.createURL('/Home'),
+      });
 
       if (createdSessionId) {
+        setLoading(false);
         setActive!({ session: createdSessionId });
       }
     } catch (err) {
@@ -41,16 +52,17 @@ const SocialAuthButton = ({ icon, provider }: SocialAuthButtonProps) => {
     }
   }, []);
 
-  const { styles } = useStyles(styleSheet);
-
+  // rendering the button
   return (
     <Pressable onPress={onPress} style={styles.container}>
-      <Image source={icon} style={styles.container} resizeMode="contain" />
+      {loading ? (
+        <ActivityIndicator size={'large'} color={theme.components.Text.title} />
+      ) : (
+        <Image source={icon} style={styles.container} resizeMode="contain" />
+      )}
     </Pressable>
   );
 };
-
-export default SocialAuthButton;
 
 const styleSheet = createStyleSheet((theme) => ({
   container: {
@@ -58,3 +70,5 @@ const styleSheet = createStyleSheet((theme) => ({
     width: height(4),
   },
 }));
+
+export default SocialAuthButton;
